15c37ec1f779a7d20bd4897b817ad604c5261e49
tomszociepa <tom.ociepa@gmail.com>
2023-12-05 14:30:39 +0100

    calendar-service config

--- calendar-service/src/main/java/com/tom/calendarservice/CalendarServiceApplication.java
+++ calendar-service/src/main/java/com/tom/calendarservice/CalendarServiceApplication.java
@@ -2,8 +2,10 @@ package com.tom.calendarservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CalendarServiceApplication {
 
 	public static void main(String[] args) {
