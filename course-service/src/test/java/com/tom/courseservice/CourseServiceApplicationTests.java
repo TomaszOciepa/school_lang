5e3a2e58eccb517321d0f9dcfb2077ddaf5226b1
tomszociepa <tom.ociepa@gmail.com>
2023-12-05 13:30:57 +0100

    course-service init

---
+++ course-service/src/test/java/com/tom/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.courseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
