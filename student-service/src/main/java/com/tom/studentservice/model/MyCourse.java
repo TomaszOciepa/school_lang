abb85322ab14c8739ef44d0fbdd8750f91dc8a75
tomszociepa <tom.ociepa@gmail.com>
2023-12-06 09:28:45 +0100

    MyCourse

--- student-service/src/main/java/com/tom/studentservice/model/MyCourse.java
+++ student-service/src/main/java/com/tom/studentservice/model/MyCourse.java
@@ -1,14 +1,21 @@
 package com.tom.studentservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
+@Entity
+@Table(name = "MyCourse")
 @Getter
 @Setter
 public class MyCourse {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private LocalDateTime enrollmentDate;
     private String courseName;
 
