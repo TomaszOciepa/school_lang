458633d1d7edadfc891606f3f6cc2e12a360db50
tomszociepa <tom.ociepa@gmail.com>
2024-01-13 08:58:41 +0100

    envairoment variable

--- frontend/src/app/modules/core/models/teacher.model.ts
+++ frontend/src/app/modules/core/models/teacher.model.ts
@@ -5,3 +5,15 @@ export interface TeacherResponse {
   email: string;
   status: boolean;
 }
+
+export type Postteacher = Omit<TeacherResponse, 'id' | 'status'>;
+
+export class Teacher implements TeacherResponse {
+  constructor(
+    public id: number,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public status: boolean
+  ) {}
+}
--- frontend/src/app/modules/core/services/teacher.service.ts
+++ frontend/src/app/modules/core/services/teacher.service.ts
@@ -3,32 +3,29 @@ import { Injectable } from '@angular/core';
 import { TeacherResponse } from '../models/teacher.model';
 import { Observable } from 'rxjs';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeacherService {
+  apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient, private oAuthService: OAuthService) {}
 
   getTeacher(): Observable<TeacherResponse[]> {
-    return this.http.get<TeacherResponse[]>(
-      'http://localhost:8092/teacher/all',
-      {
-        headers: {
-          Authorization: `Bearer ${this.oAuthService.getAccessToken()}`,
-        },
-      }
-    );
+    return this.http.get<TeacherResponse[]>(this.apiUrl + '/teacher/all', {
+      headers: {
+        Authorization: `Bearer ${this.oAuthService.getAccessToken()}`,
+      },
+    });
   }
 
   getTeacherById(id: number): Observable<TeacherResponse> {
-    return this.http.get<TeacherResponse>(
-      `http://localhost:8092/teacher/id/${id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${this.oAuthService.getAccessToken()}`,
-        },
-      }
-    );
+    return this.http.get<TeacherResponse>(`${this.apiUrl}/teacher/id/${id}`, {
+      headers: {
+        Authorization: `Bearer ${this.oAuthService.getAccessToken()}`,
+      },
+    });
   }
 }
