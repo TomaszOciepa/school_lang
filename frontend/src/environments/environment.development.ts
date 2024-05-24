c6b1c843ed10acafe0bfc227a5ba5e89b8890736
tomszociepa <tom.ociepa@gmail.com>
2024-05-24 08:14:07 +0200

    config keycloak path

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiUrlStudents: 'http://localhost:8091/student',
   apiUrlTeacher: 'http://localhost:8092/teacher',
+  // apiUrlTeacher: 'http://teacher-service:8092/teacher',
   apiUrlCourses: 'http://localhost:8093/course',
   // apiUrlLessons: 'http://localhost:9000/calendar-service/calendar',
   apiUrlLessons: 'http://localhost:8094/calendar',
