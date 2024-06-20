1cda37115a33c76ea57d34dbdc072b77bdc47928
tomszociepa <tom.ociepa@gmail.com>
2024-06-20 19:23:57 +0200

    gateway config

--- calendar-service/src/main/java/com/tom/calendarservice/controller/CalendarController.java
+++ calendar-service/src/main/java/com/tom/calendarservice/controller/CalendarController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH})
+//@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH})
 @AllArgsConstructor
 @RequestMapping("/calendar")
 public class CalendarController {
--- teacher-service/src/main/java/com/tom/teacherservice/controller/TeacherController.java
+++ teacher-service/src/main/java/com/tom/teacherservice/controller/TeacherController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH})
+//@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH})
 @AllArgsConstructor
 @RequestMapping("/teacher")
 public class TeacherController {
